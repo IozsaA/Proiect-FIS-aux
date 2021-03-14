4e6a9d3c7eb5f81c88deed2a1d49edad24cc9983
Iozsa <andrei.iozsa@student.upt.ro>
2021-03-14 10:22:51 +0200

    inline image

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import './App.css';
-import './TodoItem';
-import TodoItem from './TodoItem';
+import studentData from './studentData'
+import Student from './Student';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="todo-list">
-      <TodoItem img="https://images.generated.photos/j3OMunzJYEalDlJtS82WJ6CA4NkUAj8oV861KEXZ4h8/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yy/XzA0MTMwNDkuanBn.jpg" 
-                name="John" phone="0774123765" email="ion@gemail.com"/>
-                <TodoItem img="https://images.generated.photos/dUqTivpNrob7SNVc3_6iMkdk9Lis5RhH5jDcbAr34xo/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yy/XzA0OTY2NzcuanBn.jpg"
-                name="Alex" phone="0774321762" email="alex.perez@gemail.com"/>
-                <TodoItem img="https://images.generated.photos/VGixx6UdRn-Lq6xP0vnMxmM5MBEmaVhcW105RKmsSaY/rs:fit:256:256/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yy/XzAzODEzODcuanBn.jpg"
-                name="Casie" phone="0772442192" email="cassie@opera.tm"/>
-                <TodoItem img="https://images.generated.photos/3Pt0zdzk3Lglo-7hD-IrpUCo_ftklveFv8FbbaJFeeg/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yy/XzAzNTIxODMuanBn.jpg"
-                name="Roberta" phone="0774123812" email="roberta@gemail.mx"/>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      student: studentData
+    }
+  }
+  
+  render(){
+  const studentComponents = this.state.student.map(
+    student => <Student 
+                  id={student.id}
+                  img={student.img}
+                  name={student.name}
+                  phone={student.phone}
+                  email={student.email} 
+                />)
+    return (
+      <div className="todo-list">
+        {studentComponents}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/studentData.js
@@ -0,0 +1,32 @@
+const studentData = [
+    {
+        id: 1,
+        img:"https://images.generated.photos/j3OMunzJYEalDlJtS82WJ6CA4NkUAj8oV861KEXZ4h8/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yy/XzA0MTMwNDkuanBn.jpg",
+        name:"John",
+        phone:"0774123765",
+        email:"ion@gemail.com"
+    },
+    {
+        id : 2,
+        img:"https://images.generated.photos/dUqTivpNrob7SNVc3_6iMkdk9Lis5RhH5jDcbAr34xo/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yy/XzA0OTY2NzcuanBn.jpg",
+        name:"Alex",
+        phone:"0774321762",
+        email:"alex.abc@gemail.com"
+    },
+    {
+        id : 3,
+        img:"https://images.generated.photos/bEtXom9UGdb6U0yyEU692YSG848WtSUGB3RaJQZc6e8/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzAwMjg5MjUuanBn.jpg",
+        name:"Casie", 
+        phone:"0772442192", 
+        email:"cassie@opera.tm"
+    },
+    {
+        id : 4,
+        img:"https://images.generated.photos/s5GKJvnz327DEirGVfWa2khL-ZfK-O9d-DBylhKmna8/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzAzODA0MDUuanBn.jpg",
+        name:"Roberta", 
+        phone:"0774123812", 
+        email:"roberta@gemail.mx"
+    }
+]
+
+export default studentData
